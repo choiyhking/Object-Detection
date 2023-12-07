1870b4e4ca7f10e75e79d1d84ff32b13e385063f
Yunha Choi <61413047+choiyhking@users.noreply.github.com>
2023-12-07 19:36:57 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python detect.py --weights runs/exp1_yolov5s_bdd/weights/best_yolov5s_bdd.pt  --source bdd100k/images/test  --save-txt
+python3 detect.py --weights runs/exp1_yolov5s_bdd/weights/best_yolov5s_bdd.pt  --source bdd100k/images/test  --save-txt
